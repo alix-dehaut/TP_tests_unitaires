8b282167f666a21596077effedb5b7c42705dade
Alix <adehaut@darkmira.com>
2020-07-08 20:04:58 +0200

    fix auth

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,17 +5,30 @@ declare(strict_types=1);
 namespace App\DataFixtures;
 
 use App\Entity\User;
+use App\Service\TokenSerivce;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixtures extends Fixture
 {
     public const ADMIN_USER_REFERENCE = 'user1';
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         $user = new User();
         $user->setEmail('user1@gmail.com');
         $user->setPassword('password');
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'password'
+        ));
         $user->setFirstname('Didier');
         $user->setLastname("Premier");
         $user->setAge(30);
